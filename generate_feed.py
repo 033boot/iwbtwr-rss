012bbbffa9f90321ec13ba2aec4dcf84b8c51c50
boot <033boot@gmail.com>
2025-04-10 22:18:36 +0200

    Update generate_feed.py

--- generate_feed.py
+++ generate_feed.py
@@ -5,19 +5,27 @@ URL = "https://borntoworkretail.com/js/comic_settings.js"
 response = requests.get(URL)
 text = response.text
 
-entries = re.findall(
-    r'pgNum:\s*(\d+),\s*title:\s*["`](.*?)["`],\s*date:\s*writeDate\((\d+),\s*(\d+),\s*(\d+)\)', 
-    text
-)
+varstart = text.find('const pgData = [')
+text = text[varstart+16:text.find(']', varstart)]
+
+entries = re.findall(r'{.*?}', text, re.DOTALL)
 entries = [
-    (int(pgNum), title, datetime(int(year), int(month), int(day)))
-    for pgNum, title, year, month, day in entries
+    (int(re.search(r'pgNum: ([0-9]*?),', entry).group(1)),
+    (re.search(r'title: ("|`)(.*?)("|`),', entry).group(2)),
+    datetime.strptime(re.search(r'date: writeDate\((.*?)\),', entry).group(1), '%Y, %m, %d'))
+    for entry in entries
 ]
-entries.sort(key=lambda x: x[2], reverse=True)
+entries.reverse()
 
 
 rss = ET.Element("rss", version="2.0")
+ET.register_namespace("atom", "http://www.w3.org/2005/Atom")
 channel = ET.SubElement(rss, "channel")
+ET.SubElement(channel, "atom:link", {
+    "href": "https://033boot.github.io/iwbtwr-rss/rss.xml",
+    "rel": "self",
+    "type": "application/rss+xml"
+})
 ET.SubElement(channel, "title").text = "I Was Born To Work Retail"
 ET.SubElement(channel, "link").text = "https://borntoworkretail.com/"
 ET.SubElement(channel, "description").text = "Auto-generated from borntoworkretail.com/js/comic_settings.js"
